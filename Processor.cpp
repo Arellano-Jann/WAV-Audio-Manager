fa0f9f6091e112d119d000a3128c0c9b41113c7d
arellano-jann <jannrellano@nevada.unr.edu>
2021-11-04 12:08:25 -0700

    Half Implementation of Echo

--- Processor.cpp
+++ Processor.cpp
@@ -1,27 +1,40 @@
 // Name: Jann Arellano
 #include "Processor.h"
+#include <vector>
 
-    void normalization(std::vector<float> sample){
+    void normalization(){
         //Algo: The largest sample value in the data is found, and 
         //then the data is scaled so that that value is the maximum possible value. 
         //This maximizes the amplitude of the final waveform.
         
-        //Takes 
+        //Takes in a vector sample and normalizes it to max amplitude.
+        //How do we output? Does this need to be a void?
         float max = 0;
         float scale = 0;
         int scaleMax = 255;
         for (int i = 0; i < sample.end(); i++){ // find max in data sample
-            if (vec[i] > max){
-                max = vec[i];
+            if (sample[i] > max){
+                max = sample[i];
             }
         }
         scale = scaleMax/max; // finds scaleValue to normalize sample
         for (int i = 0; i < sample.end(); i++){ // scale data sample
-            vec[i] *= scale;
+            sample[i] *= scale;
         }
     }
     void echo(float scale, int delay){
-        
+        //Algo: Samples are copied, scaled, and 
+        //added to later locations in the sample buffer to create an echo effect.
+
+        //Takes in a float scale and delay. Copies a vector and combines both vectors.
+        std::vector<float> echo = sample;
+        for (int i = 0; i < sample.end(); i++){ // creates scaled echo vector
+            echo[i] = sample[i] * scale;
+        }
+
+        //how to implement the addition of the scales
+        //push_back the front of echo by delay
+        //add echo[i] to sample[i]
     }
     void gainAdjustment(float scale){
         
--- Processor.h
+++ Processor.h
@@ -4,6 +4,8 @@
 #define PROCESSOR_H
 
 class Processor{
+    std::vector<float> sample; // Taking the liberty to set this
+public:
     void normalization();
     void echo(float scale, int delay);
     void gainAdjustment(float scale);
