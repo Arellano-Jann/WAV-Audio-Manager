28659b2fe27e5dbe676d3f72fcc80edda20d545d
Arellano-Jann <jannrellano@nevada.unr.edu>
2021-11-06 20:11:02 -0700

    Processor Simplification

--- Processor.h
+++ Processor.h
@@ -5,10 +5,16 @@
 #include <vector>
 
 class Processor{
+    // processes a whole vector of samples
+    int scaleMax = 255; // this changes depending on bit depth 2^8 vs 2^16
     std::vector<float> sample; // Taking the liberty to set this
 public:
     void normalization();
     void echo(float scale, int delay);
-    void gainAdjustment(float scale);
-    float findMax(float min, float max);
+    void gainAdjustment(std::vector<float> &sample, float scale);
+
+    //Helper Functions
+    float findMax(float max);
+    float findMin(float min);
+    float findScale(float min, float max, float scale);
 };
