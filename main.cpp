6ea4a260cfbf74580f136df68493c671ac2992fd
Alec Redera <alecredera@Alecs-MacBook-Pro.local>
2021-11-09 14:07:37 -0800

    main function added, terminal-type ui, needs a loop function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
-// Name:
+// Created by Alec Redera
 
-
+#include <iostream>
+#include <string>
 #include "Wav.h"
+
+std::string InputVariable;
+
+int main()
+{
+    std::cout << "This Program Can Load and Modify WAV Files." << std::endl;
+    std::cout << "To Begin, Please Enter the Name of a WAV File, or Enter \"Quit\" to Exit the Program." << std::endl;
+    std::cout << "Enter WAV Filename:" << std::endl;
+
+    std::cin >> InputVariable;
+
+    if (InputVariable == "QUIT" || "Quit" || "quit") {
+        std::cout << "Goodbye!" << std::endl;
+    }
+
+    else {
+        //Not sure which function I'm supposed to lead into. I think Andrew's?
+    }
+
+return 0;
+}
+
+//Not sure what this function in main did, or if it should be in a separate file.
+/*
 int main()
 {
     Wav w;
@@ -14,3 +39,4 @@ int main()
     
     return 0;
 }
+*/
