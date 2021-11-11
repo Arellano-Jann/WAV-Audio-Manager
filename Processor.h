89548e1ae708367dcfcc78bc02c6dc0fd39bafb9
Arellano-Jann <jannrellano@nevada.unr.edu>
2021-11-11 13:08:37 -0800

    Comments

--- Processor.h
+++ Processor.h
@@ -4,6 +4,14 @@
 #define PROCESSOR_H
 #include <vector>
 
+// possibly change all methods to take in a sample
+// i think this is because i intended to use vector.normalization()
+// but vector does not have normalization method
+// you can only use dot operator on methods when the object has the method
+// vector does not have normalization method
+// so configure to take in a vector instead of intending it to use a dot operator
+// ^^ see if it works first because it should be an easy fix
+
 class Processor{
     // processes a whole vector of samples
     int scaleMax = 255; // this changes depending on bit depth 2^8 vs 2^16
