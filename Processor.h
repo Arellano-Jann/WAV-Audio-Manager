f7524680744f955a09134db2144170c82b3e7edb
Arellano-Jann <jannrellano@nevada.unr.edu>
2021-11-19 21:31:56 -0800

    Fix back to vectors

--- Processor.h
+++ Processor.h
@@ -3,25 +3,26 @@
 #pragma once
 #define PROCESSOR_H
 #include <iostream>
-#include "WaveHeader.h"
+#include <vector>
 
 class Processor{
     // processes a whole vector of samples
     int scaleMax = 255; // this changes depending on bit depth 2^8 vs 2^16
-    WaveHeader header;
-    float sample[header.dataBytes];
+    std::vector<float> sample; // this works by having a constructor. 
+    // the wav is casted as a processor object now.
+    // need to have extra method so that 
 public:
-    void normalization(float (&sample)[]);
-    // void echo();
-    // void gainAdjustment();
-    // void gainAdjustment(float scale);
-    // void lowPassFilter();
-    // void compression();
+    Processor(std::vector<float> sample) : sample(sample){}
+    void normalization();
+    void echo();
+    void gainAdjustment();
+    void gainAdjustment(float scale);
+    void lowPassFilter();
+    void compression();
 
     //Helper Functions
-    float findMax(float (&sample)[], float max);
-    float findMin(float (&sample)[], float min);
-    float findScale(float min, float max, float scale);
+    float findMax();
+    float findMin();
+    float findScale(float min, float max);
     float ask(std::string question);
-    int size(float arr[]);
 };
