45c1903c3bbd45eba9eb8c6523a9f6452b23d0ef
Arellano-Jann <jannrellano@nevada.unr.edu>
2021-12-01 23:36:52 -0800

    Smart Pointer Conversion. This does not fix it

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
 #include "../headers/UI.h"
 #include "../headers/Wav.h"
 #include "../headers/Processor.h"
@@ -52,13 +53,15 @@ int main()
             int selection = ui.selectProcessor();
             
             // Processing
-            Processor p(wav.GetSamples());
+            std::shared_ptr<Processor> p{new Processor (wav.GetSamples())};
+            // Processor p(wav.GetSamples());
             switch (selection){
                 float parameterOne;
                 float parameterTwo;
                 case 1: 
                     std::cout << "is this where you're breaking 60";
-                    p = Normalization(wav.GetSamples());
+                    p = std::shared_ptr<Processor> {new Normalization(wav.GetSamples())};
+                    // p = Normalization(wav.GetSamples());
                     std::cout << "is this where you're breaking 62";
                     break;
                 case 2:
@@ -66,24 +69,28 @@ int main()
                     std::cin >> parameterOne;
                     ui.askProcessorQuestions(2);
                     std::cin >> parameterTwo;
-                    p = Echo(wav.GetSamples(), parameterOne, parameterTwo);
+                    p = std::shared_ptr<Processor> {new Echo(wav.GetSamples(), parameterOne, parameterTwo)};
+                    // p = Echo(wav.GetSamples(), parameterOne, parameterTwo);
                     break;
                 case 3:
                     ui.askProcessorQuestions(1);
                     std::cin >> parameterOne;
-                    p = Gain(wav.GetSamples(), parameterOne);
+                    p = std::shared_ptr<Processor> {new Gain(wav.GetSamples(), parameterOne)};
+                    // p = Gain(wav.GetSamples(), parameterOne);
                     break;
                 case 4:
                     ui.askProcessorQuestions(5);
                     std::cin >> parameterOne;
-                    p = LowPassFilter(wav.GetSamples(), parameterOne);
+                    p = std::shared_ptr<Processor> {new LowPassFilter(wav.GetSamples(), parameterOne)};
+                    // p = LowPassFilter(wav.GetSamples(), parameterOne);
                     break;
                 case 5:
                     ui.askProcessorQuestions(3);
                     std::cin >> parameterOne;
                     ui.askProcessorQuestions(4);
                     std::cin >> parameterTwo;
-                    p = Compressor(wav.GetSamples(), parameterOne, parameterTwo);
+                    p = std::shared_ptr<Processor> {new Compressor(wav.GetSamples(), parameterOne, parameterTwo)};
+                    // p = Compressor(wav.GetSamples(), parameterOne, parameterTwo);
                     break;
                 case 6:
                     // std::string output = ui.OutputFileName();
