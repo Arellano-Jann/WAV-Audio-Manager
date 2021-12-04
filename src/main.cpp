4d37963750df9d940985596098e9fd2629269115
Arellano-Jann <jannrellano@nevada.unr.edu>
2021-12-03 20:00:12 -0800

    Cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -33,82 +33,82 @@ int main()
 
         bool done = false;
         std::shared_ptr<Processor> p{new Processor (wav.GetSamples())};
-        // while(!done)
-        // {
+        while(!done)
+        {
             ui.ProcessorMenu();
-            // int selection = ui.selectProcessor(); // uncomment
+            int selection = ui.selectProcessor(); // uncomment
             
             // Processing
 
             // Processor p(wav.GetSamples());
             // std::cout << "is this where you're breaking 60";
-                    p = std::shared_ptr<Processor> {new Normalization(wav.GetSamples())};
+                    // p = std::shared_ptr<Processor> {new Normalization(wav.GetSamples())};
                     // p = Normalization(wav.GetSamples());
                     // std::cout << "is this where you're breaking 62";
                     
 
-        //     switch (selection){
-        //         float parameterOne;
-        //         float parameterTwo;
-        //         case 1: 
-        //             p = std::shared_ptr<Processor> {new Normalization(wav.GetSamples())};
-        //             // p = Normalization(wav.GetSamples());
-        //             break;
-        //         case 2:
-        //             ui.askProcessorQuestions(1);
-        //             std::cin >> parameterOne;
-        //             ui.askProcessorQuestions(2);
-        //             std::cin >> parameterTwo;
-        //             p = std::shared_ptr<Processor> {new Echo(wav.GetSamples(), parameterOne, parameterTwo)};
-        //             // p = Echo(wav.GetSamples(), parameterOne, parameterTwo);
-        //             break;
-        //         case 3:
-        //             ui.askProcessorQuestions(1);
-        //             std::cin >> parameterOne;
-        //             for (auto x : p->getSample()){
-        //             std::cout << x << " ";
-        //             break;
-        //             }
-        //             p = std::shared_ptr<Processor> {new Gain(wav.GetSamples(), parameterOne)};
-        //             wav.setSamples(p->getSample());
-        //             for (auto x : wav.GetSamples()){
-        //             std::cout << x << " ";
-        //             break;
-        //             }
-        //             std::cout << std::endl;
-        //             for (auto x : p->getSample()){
-        //             std::cout << x << " ";
-        //             break;
-        //             }
-        //             // p = Gain(wav.GetSamples(), parameterOne);
-        //             break;
-        //         case 4:
-        //             ui.askProcessorQuestions(5);
-        //             std::cin >> parameterOne;
-        //             p = std::shared_ptr<Processor> {new LowPassFilter(wav.GetSamples(), parameterOne)};
-        //             // p = LowPassFilter(wav.GetSamples(), parameterOne);
-        //             break;
-        //         case 5:
-        //             ui.askProcessorQuestions(3);
-        //             std::cin >> parameterOne;
-        //             ui.askProcessorQuestions(4);
-        //             std::cin >> parameterTwo;
-        //             p = std::shared_ptr<Processor> {new Compressor(wav.GetSamples(), parameterOne, parameterTwo)};
-        //             // p = Compressor(wav.GetSamples(), parameterOne, parameterTwo);
-        //             // wav.setSamples(p->getSample());
-        //             break;
-        //         case 6: // consider putting this outside the switch.
-        //             std::string output = ui.OutputFileName();
-        //             wav.setSamples(p->getSample());
-        //             wav.CreateFile(output);
-        //             done = true;
-        //             break;
-        //     }
-        // }
+            switch (selection){
+                float parameterOne;
+                float parameterTwo;
+                case 1: 
+                    p = std::shared_ptr<Processor> {new Normalization(wav.GetSamples())};
+                    // p = Normalization(wav.GetSamples());
+                    break;
+                case 2:
+                    ui.askProcessorQuestions(1);
+                    std::cin >> parameterOne;
+                    ui.askProcessorQuestions(2);
+                    std::cin >> parameterTwo;
+                    p = std::shared_ptr<Processor> {new Echo(wav.GetSamples(), parameterOne, parameterTwo)};
+                    // p = Echo(wav.GetSamples(), parameterOne, parameterTwo);
+                    break;
+                case 3:
+                    ui.askProcessorQuestions(1);
+                    std::cin >> parameterOne;
+                    for (auto x : p->getSample()){
+                    std::cout << x << " ";
+                    break;
+                    }
+                    p = std::shared_ptr<Processor> {new Gain(wav.GetSamples(), parameterOne)};
+                    wav.setSamples(p->getSample());
+                    for (auto x : wav.GetSamples()){
+                    std::cout << x << " ";
+                    break;
+                    }
+                    std::cout << std::endl;
+                    for (auto x : p->getSample()){
+                    std::cout << x << " ";
+                    break;
+                    }
+                    // p = Gain(wav.GetSamples(), parameterOne);
+                    break;
+                case 4:
+                    ui.askProcessorQuestions(5);
+                    std::cin >> parameterOne;
+                    p = std::shared_ptr<Processor> {new LowPassFilter(wav.GetSamples(), parameterOne)};
+                    // p = LowPassFilter(wav.GetSamples(), parameterOne);
+                    break;
+                case 5:
+                    ui.askProcessorQuestions(3);
+                    std::cin >> parameterOne;
+                    ui.askProcessorQuestions(4);
+                    std::cin >> parameterTwo;
+                    p = std::shared_ptr<Processor> {new Compressor(wav.GetSamples(), parameterOne, parameterTwo)};
+                    // p = Compressor(wav.GetSamples(), parameterOne, parameterTwo);
+                    // wav.setSamples(p->getSample());
+                    break;
+                case 6: // consider putting this outside the switch.
+                    std::string output = ui.OutputFileName();
+                    wav.setSamples(p->getSample());
+                    wav.CreateFile(output);
+                    done = true;
+                    break;
+            }
+        }
 
-        std::string output = ui.OutputFileName();
+        // std::string output = ui.OutputFileName();
         // wav.setSamples(p->getSample()); // same as below?
-        wav.CreateFile(output);
+        // wav.CreateFile(output);
 
     }
     else
